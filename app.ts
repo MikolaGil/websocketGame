2e322ebed908a75f74f1d7940ba3657f0b644db6
MikolaGil <mikolagil@gmail.com>
2022-04-02 13:47:09 +0200

    added icon new styles

--- app.ts
+++ app.ts
@@ -7,10 +7,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use("/public", express.static("public"));
+
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../views/html/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`Server is running https://localhost:${port}`);
 });
--- dist/app.js
+++ dist/app.js
@@ -9,9 +9,10 @@ const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use("/public", express_1.default.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '../views/html/index.html'));
 });
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+    console.log(`Server is running https://localhost:${port}`);
 });
